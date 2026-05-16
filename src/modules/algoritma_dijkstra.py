61418b4aa2d8cdfe2ebf90ca054934b41a00ced3
muhammadyaafi <muhammadyaafi.2025@student.uny.ac.id>
2026-05-16 21:16:36 +0700

    update algoritma_dijkstra.py

--- src/modules/algoritma_dijkstra.py
+++ src/modules/algoritma_dijkstra.py
@@ -1,9 +1,47 @@
 def dijkstra_logistik(graph, depot): 
     """Shortest path dari depot. Big-O: O(V^2+E).""" 
     INF = float('inf') 
-    dist = {v: INF for v in graph.adj} 
-    parent = {v: None for v in graph.adj} 
-    dist[depot] = 0 
-    visited = set() 
+    dist = {v: INF for v in graph.adj} # Jarak awal semua node = tak hingga
+    parent = {v: None for v in graph.adj} # Parent untuk rekonstruksi jalur
+    dist[depot] = 0 # Jarak depot ke dirinya sendiri = 0
+    visited = set() # Menyimpan node yang sudah diproses
     # TODO: implementasikan
-    return dist, parent 
+
+    while len(visited) < len(graph.adj):
+
+        # Cari node belum dikunjungi
+        # dengan jarak minimum
+        current = None
+        current_dist = INF
+
+        for node in graph.adj:
+
+            if node not in visited and dist[node] < current_dist:
+                current = node
+                current_dist = dist[node]
+
+        # Jika tidak ada node terjangkau lagi
+        if current is None:
+            break
+
+        # Tandai sudah dikunjungi
+        visited.add(current)
+
+        # Traversal tetangga current
+        edge = graph.adj[current]
+
+        while edge:
+
+            neighbor = edge.dest
+            weight = edge.jarak
+
+            # Relaxation
+            new_dist = dist[current] + weight
+
+            if new_dist < dist[neighbor]:
+                dist[neighbor] = new_dist
+                parent[neighbor] = current
+
+            edge = edge.next
+
+    return dist, parent
